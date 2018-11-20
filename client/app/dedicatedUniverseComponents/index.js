c03a4a247917e5ccceaccc9f703bd3a847ab6312
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-11-20 15:13:30 +0100

    refactor(components.tabs): move to dedicatedUniverseComponents

--- client/app/dedicatedUniverseComponents/index.js
+++ client/app/dedicatedUniverseComponents/index.js
@@ -1,9 +1,12 @@
 import angular from 'angular';
 
+import ducTabs from './tabs';
+
 const moduleName = 'dedicatedUniverseComponents';
 
 angular
   .module(moduleName, [
+    ducTabs,
   ]);
 
 export default moduleName;
